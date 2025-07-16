9fa2478ffa3c0c71653bc6740dfc4a6a4a0e52b8
AyushPai4159 <ayushpai030304@gmail.com>
2025-07-16 00:50:13 -0400

    integrated 0.3 for frontend and backend

---
+++ backend/pythonFiles/createDoc.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+import json
+import numpy as np
+
+with open("../data/output.json", "r") as file:
+    data = json.load(file)
+
+
+keys = list(data.keys())
+
+
+
+model = SentenceTransformer('../my_model_dir')
+
+doc_embeddings = model.encode(keys, convert_to_tensor=True)
+np.save("../data/doc_embeddings.npy", doc_embeddings)
